0e4fbf19ea2553f4e01b0fd14c7ca65b86b1dae1
EmersonCDias <emerson_cdias@hotmail.com>
2021-01-03 12:53:14 -0300

    Improving routes and controller logic and finishing backend

--- src/modules/appointments/infra/http/routes/providers.route.ts
+++ src/modules/appointments/infra/http/routes/providers.route.ts
@@ -3,12 +3,24 @@ import { Router } from 'express';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 import ProvidersController from '../controllers/ProvidersController';
+import ListProviderDayAvailabilityService from '../controllers/ListProviderDayAvailabilityController';
+import ListProviderMonthAvailabilityService from '../controllers/ListProviderMonthAvailabilityController';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
+const listProviderDayAvailabilityService = new ListProviderDayAvailabilityService();
+const listProviderMonthAvailabilityService = new ListProviderMonthAvailabilityService();
 
 providersRouter.use(ensureAuthenticated);
 
 providersRouter.get('/', providersController.index);
+providersRouter.get(
+  '/:provider_id/day-availability',
+  listProviderDayAvailabilityService.index,
+);
+providersRouter.get(
+  '/:provider_id/month-availability',
+  listProviderMonthAvailabilityService.index,
+);
 
 export default providersRouter;
