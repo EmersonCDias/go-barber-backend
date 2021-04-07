3130d7d5a0c0d30eabe970e2414dcef353e63619
EmersonCDias <emerson_cdias@hotmail.com>
2021-04-07 14:26:03 -0300

    Testing

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,36 +1,35 @@
 // import 'reflect-metadata';
 // import 'dotenv/config';
 import express, { Request, Response, NextFunction, response } from 'express';
-// import express, { Request, Response, response } from 'express';
-// import 'express-async-errors';
-// import cors from 'cors';
-// import { errors } from 'celebrate';
-//
-// import uploadConfig from '../../../config/upload';
-import AppErrors from '../../errors/AppErrors';
+import 'express-async-errors';
+import cors from 'cors';
+import { errors } from 'celebrate';
 
-// import rateLimiter from './middlewares/rateLimiter';
-// import routes from './routes';
-//
-// import '../typeorm';
-// import '../../container';
+import uploadConfig from '../../../config/upload';
+// import AppErrors from '../../errors/AppErrors';
+
+import rateLimiter from './middlewares/rateLimiter';
+import routes from './routes';
+
+import '../typeorm';
+import '../../container';
 //
 const app = express();
 
-// app.use(cors());
-// app.use(express.json());
-// app.use('/files', express.static(uploadConfig.uploadsFolder));
-// app.use(rateLimiter);
-// app.use(routes);
-// app.use(errors());
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(uploadConfig.uploadsFolder));
+app.use(rateLimiter);
+app.use(routes);
+app.use(errors());
 //
 app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
-  if (err instanceof AppErrors) {
-    return res.status(err.statusCode).json({
-      status: 'error',
-      msg: err.msg,
-    });
-  }
+  // if (err instanceof AppErrors) {
+  //   return res.status(err.statusCode).json({
+  //     status: 'error',
+  //     msg: err.msg,
+  //   });
+  // }
 
   console.log('req', req);
   console.log('res', res);
