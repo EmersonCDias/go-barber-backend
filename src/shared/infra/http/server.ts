b829d577b1dd2de1b7ba528422e53b56ccc9eaaa
EmersonCDias <emerson_cdias@hotmail.com>
2021-04-07 14:18:49 -0300

    Testing

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,7 +1,7 @@
 // import 'reflect-metadata';
 // import 'dotenv/config';
-// import express, { Request, Response, NextFunction, response } from 'express';
-import express, { Request, Response, response } from 'express';
+import express, { Request, Response, NextFunction, response } from 'express';
+// import express, { Request, Response, response } from 'express';
 // import 'express-async-errors';
 // import cors from 'cors';
 // import { errors } from 'celebrate';
@@ -24,31 +24,34 @@ const app = express();
 // app.use(routes);
 // app.use(errors());
 //
-// app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
-//   if (err instanceof AppErrors) {
-//     return res.status(err.statusCode).json({
-//       status: 'error',
-//       msg: err.msg,
-//     });
-//   }
-//
-//   return response.status(500).json({
-//     status: 'error',
-//     msg: 'Internal server erro',
-//   });
-// });
-//
-// app.listen(process.env.PORT || 3333);
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  // if (err instanceof AppErrors) {
+  //   return res.status(err.statusCode).json({
+  //     status: 'error',
+  //     msg: err.msg,
+  //   });
+  // }
 
-const PORT: string | number = process.env.PORT || 5000;
-
-app.use('*', (req: Request, res: Response) => {
-  console.log('res', req);
+  console.log('req', req);
   console.log('res', res);
-  response.status(500).json({
+
+  return response.status(500).json({
     status: 'error',
     msg: 'Internal server error 1',
   });
 });
 
-app.listen(PORT, () => console.log(`hosting @${PORT}`));
+app.listen(process.env.PORT || 3333);
+
+// const PORT: string | number = process.env.PORT || 5000;
+//
+// app.use('*', (req: Request, res: Response) => {
+//   console.log('res', req);
+//   console.log('res', res);
+//   response.status(500).json({
+//     status: 'error',
+//     msg: 'Internal server error 1',
+//   });
+// });
+//
+// app.listen(PORT, () => console.log(`hosting @${PORT}`));
