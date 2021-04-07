02f22b3447560dafb1771179f0bedb485f20ef55
EmersonCDias <emerson_cdias@hotmail.com>
2021-04-07 14:29:06 -0300

    Testing

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,26 +3,26 @@
 import express, { Request, Response, NextFunction, response } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
-import { errors } from 'celebrate';
+// import { errors } from 'celebrate';
 
-import uploadConfig from '../../../config/upload';
+// import uploadConfig from '../../../config/upload';
 // import AppErrors from '../../errors/AppErrors';
 
-import rateLimiter from './middlewares/rateLimiter';
-import routes from './routes';
+// import rateLimiter from './middlewares/rateLimiter';
+// import routes from './routes';
 
-import '../typeorm';
-import '../../container';
-//
+// import '../typeorm';
+// import '../../container';
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.uploadsFolder));
-app.use(rateLimiter);
-app.use(routes);
-app.use(errors());
-//
+// app.use('/files', express.static(uploadConfig.uploadsFolder));
+// app.use(rateLimiter);
+// app.use(routes);
+// app.use(errors());
+
 app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
   // if (err instanceof AppErrors) {
   //   return res.status(err.statusCode).json({
