66ba092b5238415d8c87e99cb698df7045eb9b2c
EmersonCDias <emerson_cdias@hotmail.com>
2021-04-07 13:37:12 -0300

    Testing

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,45 +1,56 @@
-/* eslint-disable no-console */
-import 'reflect-metadata';
-import 'dotenv/config';
-import express, { Request, Response, NextFunction, response } from 'express';
-import 'express-async-errors';
-import cors from 'cors';
-import { errors } from 'celebrate';
-
-import uploadConfig from '../../../config/upload';
-import AppErrors from '../../errors/AppErrors';
-
-import rateLimiter from './middlewares/rateLimiter';
-import routes from './routes';
-
-import '../typeorm';
-import '../../container';
+import express from 'express';
 
 const app = express();
+const PORT: string | number = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
-app.use('/files', express.static(uploadConfig.uploadsFolder));
-app.use(rateLimiter);
-app.use(routes);
-app.use(errors());
-
-app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
-  if (err instanceof AppErrors) {
-    return res.status(err.statusCode).json({
-      status: 'error',
-      msg: err.msg,
-    });
-  }
-
-  console.log('==========>', err);
-
-  return response.status(500).json({
-    status: 'error',
-    msg: 'Internal server erro',
-  });
+app.use('*', (req, res) => {
+  res.send('<h1>Welcome to your simple server! Awesome right</h1>');
 });
 
-app.listen(process.env.PORT || 3333, () =>
-  console.log('Server running at port 3333!'),
-);
+app.listen(PORT, () => console.log(`hosting @${PORT}`));
+
+/* eslint-disable no-console */
+// import 'reflect-metadata';
+// import 'dotenv/config';
+// import express, { Request, Response, NextFunction, response } from 'express';
+// import 'express-async-errors';
+// import cors from 'cors';
+// import { errors } from 'celebrate';
+//
+// import uploadConfig from '../../../config/upload';
+// import AppErrors from '../../errors/AppErrors';
+
+// import rateLimiter from './middlewares/rateLimiter';
+// import routes from './routes';
+//
+// import '../typeorm';
+// import '../../container';
+//
+// const app = express();
+//
+// app.use(cors());
+// app.use(express.json());
+// app.use('/files', express.static(uploadConfig.uploadsFolder));
+// app.use(rateLimiter);
+// app.use(routes);
+// app.use(errors());
+//
+// app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+//   if (err instanceof AppErrors) {
+//     return res.status(err.statusCode).json({
+//       status: 'error',
+//       msg: err.msg,
+//     });
+//   }
+//
+//   console.log('==========>', err);
+//
+//   return response.status(500).json({
+//     status: 'error',
+//     msg: 'Internal server erro',
+//   });
+// });
+//
+// app.listen(process.env.PORT || 3333, () =>
+//   console.log('Server running at port 3333!'),
+// );
